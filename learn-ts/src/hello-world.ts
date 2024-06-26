9d04a3f2a58253514ca1dfb8d0d955d002058685
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-06-26 15:41:09 +0530

    src: run above the powerful type vaidation in ts

--- learn-ts/src/hello-world.ts
+++ learn-ts/src/hello-world.ts
@@ -14,3 +14,16 @@ const pikachu = {
 console.log(typeof pikachu);
 
 console.log(pikachu.weight);
+
+//backand forth communication between editor and us, lets us help this in developing this further.
+//typescript types can help in setting the types and the editor and not figure everyhting out at the runtime.
+
+const logPokemon = (pokemons: string[] | null) => {
+  if (typeof pokemons === "object") {
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    pokemons?.forEach((pokemon) => console.log(pokemon));
+  }
+  return pokemon;
+};
+
+logPokemon(["Bulbasuar", "Charmander", "Squirtle"]);
